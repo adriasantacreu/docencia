dfa723a54ea1b25785b1e4840ec6943fcccddc84
Adrià Santacreu i Giménez <adriasantacreu@gmail.com>
2026-02-05 20:19:35 +0100

    Inicialització del portafoli docent amb Astro Sphere

---
+++ src/consts.ts
@@ -0,0 +1,85 @@
+import type { Site, Page, Links, Socials } from "@types"
+
+// Global
+export const SITE: Site = {
+  TITLE: "Portafoli Docència",
+  DESCRIPTION: "Benvingut al meu portafoli personal i de docència.",
+  AUTHOR: "Adrià Santacreu",
+}
+
+// Work Page
+export const WORK: Page = {
+  TITLE: "Experiència",
+  DESCRIPTION: "Llocs on he treballat.",
+}
+
+// Blog Page
+export const BLOG: Page = {
+  TITLE: "Blog",
+  DESCRIPTION: "Escrits sobre temes que m'apassionen.",
+}
+
+// Projects Page 
+export const PROJECTS: Page = {
+  TITLE: "Projectes",
+  DESCRIPTION: "Projectes recents en els que he treballat.",
+}
+
+// Search Page
+export const SEARCH: Page = {
+  TITLE: "Cercar",
+  DESCRIPTION: "Cerca tots els articles i projectes.",
+}
+
+// Links
+export const LINKS: Links = [
+  {
+    TEXT: "Inici",
+    HREF: "/",
+  },
+  {
+    TEXT: "Experiència",
+    HREF: "/work",
+  },
+  {
+    TEXT: "Blog",
+    HREF: "/blog",
+  },
+  {
+    TEXT: "Projectes",
+    HREF: "/projects",
+  },
+  {
+    TEXT: "Generador",
+    HREF: "/generador",
+  },
+]
+
+// Socials
+export const SOCIALS: Socials = [
+  {
+    NAME: "Email",
+    ICON: "email",
+    TEXT: "adria.santacreu@gmail.com",
+    HREF: "mailto:adria.santacreu@gmail.com",
+  },
+  {
+    NAME: "Github",
+    ICON: "github",
+    TEXT: "markhorn-dev",
+    HREF: "https://github.com/markhorn-dev/astro-sphere"
+  },
+  {
+    NAME: "LinkedIn",
+    ICON: "linkedin",
+    TEXT: "markhorn-dev",
+    HREF: "https://www.linkedin.com/in/markhorn-dev/",
+  },
+  {
+    NAME: "Twitter",
+    ICON: "twitter-x",
+    TEXT: "markhorn_dev",
+    HREF: "https://twitter.com/markhorn_dev",
+  },
+]
+
